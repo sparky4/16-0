7995edf3e396015679b1a3ece7a01a94dde8d056
sparky4 <sparky4@cock.li>
2023-10-18 00:17:12 -0500

    removed unused code snippet from 16_in.c

--- src/lib/16_in.c
+++ src/lib/16_in.c
@@ -120,12 +120,6 @@ extern "C" {
 	char		LastASCII;
 	ScanCode	LastScan;
 
-//the def stuff is need fix warnings
-#ifndef INSTDEFED
-#define INSTDEFED
-//static inst_t inst;
-#endif
-
 static	byte        far ASCIINames[] =		// Unshifted ASCII for scan codes
 					{
 //	 0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
